7d46815cc5fd6ee1a3e3e75a710f1d84c01aaa99
Gretchen Krammes <gkkrammes@gmail.com>
2020-09-30 20:09:08 -0400

    Created database and tables. Rendered post.

--- schema.sql
+++ schema.sql
@@ -2,3 +2,11 @@
 DROP DATABASE IF EXISTS burger_db;
 
 CREATE DATABASE burger_db;
+
+REATE TABLE burgers (
+  id int NOT NULL AUTO_INCREMENT,
+  burger varchar(255) NOT NULL,
+  PRIMARY KEY (id)
+);
+
+INSERT INTO burgers (burger) VALUES ('?');
--- server.js
+++ server.js
@@ -13,3 +13,45 @@ app.use(express.json());
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "Lukenelson12!",
+    database: "burger_db"
+  });
+
+  connection.connect(function(err) {
+    if (err) {
+      console.error("error connecting: " + err.stack);
+      return;
+    }
+  });
+    //render main index.html
+  app.get("/", function(req, res) {
+    connection.query("SELECT * FROM burgers;", function(err, burgers) {
+      if (err) {
+        return res.status(500).end();
+      }
+  
+      res.render("index", { burgers });
+    });
+  });
+    //Create a new burger
+  app.post("/api/burgers", function(req, res) {
+    connection.query("INSERT INTO burgers (burger) VALUES (?)", [req.body.burger], function(err, result) {
+      if (err) {
+        return res.status(500).end();
+      }
+  
+      // Send the id of a new burger back
+      res.json({ id: result.insertId });
+      console.log({ id: result.insertId });
+    });
+  });
+
+  app.listen(PORT, function() {
+    // Log (server-side) when our server has started
+    console.log("Server listening on: http://localhost:" + PORT);
+  });
+  
